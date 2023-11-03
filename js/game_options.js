f9e14915852ceeb514440b9f4183c35b1f0c0c1e
TobiLean <Tobiloba.tsl@gmail.com>
2023-11-03 18:00:48 +0400

    Beatified website and added necessary comments

--- js/game_options.js
+++ js/game_options.js
@@ -4,111 +4,115 @@ var start_end = document.getElementById("start_end_btn");
 var cells_light = Array.from(document.getElementsByClassName("cell light"));
 var cells_dark = Array.from(document.getElementsByClassName("cell dark"));
 
-modes[0].addEventListener("click", ()=>{
+//checks for button click on first mode button and runs a function
+modes[0].addEventListener("click", () => {
+  setTimeout(function () {
+    modes[0].disabled = true;
+    modes[1].disabled = false;
+    modes[2].disabled = false;
+  }, 0);
 
-    setTimeout(function() {
-        modes[0].disabled = true
-        modes[1].disabled = false
-        modes[2].disabled = false
-    }, 0)
+  modes[0].setAttribute("style", "border: 1px solid #072541;");
+  modes[1].setAttribute("style", "border: 0");
+  modes[2].setAttribute("style", "border: 0");
+});
 
-    modes[0].setAttribute("style", "border: 1px solid #072541;");
-    modes[1].setAttribute("style", "border: 0");
-    modes[2].setAttribute("style", "border: 0");
-})
+//checks for button click on second mode button and runs a function
+modes[1].addEventListener("click", () => {
+  setTimeout(function () {
+    modes[1].disabled = true;
+    modes[0].disabled = false;
+    modes[2].disabled = false;
+  }, 0);
 
-modes[1].addEventListener("click", ()=>{
-    setTimeout(function() {
-        modes[1].disabled = true
-        modes[0].disabled = false
-        modes[2].disabled = false
-    }, 0)
+  modes[1].setAttribute("style", "border: 1px solid #072541;");
+  modes[0].setAttribute("style", "border: 0");
+  modes[2].setAttribute("style", "border: 0");
+});
 
-    modes[1].setAttribute("style", "border: 1px solid #072541;");
-    modes[0].setAttribute("style", "border: 0");
-    modes[2].setAttribute("style", "border: 0");
-})
+//checks for button click on third mode button and runs a function
+modes[2].addEventListener("click", () => {
+  setTimeout(function () {
+    modes[2].disabled = true;
+    modes[1].disabled = false;
+    modes[0].disabled = false;
+  }, 0);
 
-modes[2].addEventListener("click", ()=>{
-    setTimeout(function() {
-        modes[2].disabled = true
-        modes[1].disabled = false
-        modes[0].disabled = false
-    }, 0)
+  modes[2].setAttribute("style", "border: 1px solid #072541;");
+  modes[1].setAttribute("style", "border: 0");
+  modes[0].setAttribute("style", "border: 0");
+});
 
-    modes[2].setAttribute("style", "border: 1px solid #072541;");
-    modes[1].setAttribute("style", "border: 0");
-    modes[0].setAttribute("style", "border: 0");
-})
+//checks for button click on first theme button and runs a function
+themes[0].addEventListener("click", () => {
+  setTimeout(function () {
+    themes[0].disabled = true;
+    themes[1].disabled = false;
+    themes[2].disabled = false;
+  }, 0);
 
-themes[0].addEventListener("click", ()=>{
+  themes[0].setAttribute("style", "border: 1px solid #072541;");
+  themes[1].setAttribute("style", "border: 0");
+  themes[2].setAttribute("style", "border: 0");
 
-    setTimeout(function() {
-        themes[0].disabled = true
-        themes[1].disabled = false
-        themes[2].disabled = false
-    }, 0)
+  for (i = 0; i < cells_light.length; i++) {
+    cells_light[i].setAttribute("style", "background-color: white");
+  }
 
-    themes[0].setAttribute("style", "border: 1px solid #072541;");
-    themes[1].setAttribute("style", "border: 0");
-    themes[2].setAttribute("style", "border: 0");
+  for (i = 0; i < cells_dark.length; i++) {
+    cells_dark[i].setAttribute("style", "background-color: black");
+  }
+});
 
-    for(i=0; i<cells_light.length; i++){
-        cells_light[i].setAttribute("style", "background-color: white");
-    }
+//checks for button click on second theme button and runs a function
+themes[1].addEventListener("click", () => {
+  setTimeout(function () {
+    themes[1].disabled = true;
+    themes[0].disabled = false;
+    themes[2].disabled = false;
+  }, 0);
 
-    for(i=0; i<cells_dark.length; i++){
-        cells_dark[i].setAttribute("style", "background-color: black");
-    }
-})
+  themes[1].setAttribute("style", "border: 1px solid #072541;");
+  themes[0].setAttribute("style", "border: 0");
+  themes[2].setAttribute("style", "border: 0");
 
-themes[1].addEventListener("click", ()=>{
-    setTimeout(function() {
-        themes[1].disabled = true
-        themes[0].disabled = false
-        themes[2].disabled = false
-    }, 0)
+  for (i = 0; i < cells_light.length; i++) {
+    cells_light[i].setAttribute("style", "background-color: #92ef67");
+  }
 
-    themes[1].setAttribute("style", "border: 1px solid #072541;");
-    themes[0].setAttribute("style", "border: 0");
-    themes[2].setAttribute("style", "border: 0");
+  for (i = 0; i < cells_dark.length; i++) {
+    cells_dark[i].setAttribute("style", "background-color: #00682c");
+  }
+});
 
-    for(i=0; i<cells_light.length; i++){
-        cells_light[i].setAttribute("style", "background-color: #92ef67");
-    }
+//checks for button click on third theme button and runs a function
+themes[2].addEventListener("click", () => {
+  setTimeout(function () {
+    themes[2].disabled = true;
+    themes[1].disabled = false;
+    themes[0].disabled = false;
+  }, 0);
 
-    for(i=0; i<cells_dark.length; i++){
-        cells_dark[i].setAttribute("style", "background-color: #00682c");
-    }
-})
+  themes[2].setAttribute("style", "border: 1px solid #072541;");
+  themes[1].setAttribute("style", "border: 0");
+  themes[0].setAttribute("style", "border: 0");
 
-themes[2].addEventListener("click", ()=>{
-    setTimeout(function() {
-        themes[2].disabled = true
-        themes[1].disabled = false
-        themes[0].disabled = false
-    }, 0)
+  for (i = 0; i < cells_light.length; i++) {
+    cells_light[i].setAttribute("style", "background-color: white");
+  }
 
-    themes[2].setAttribute("style", "border: 1px solid #072541;");
-    themes[1].setAttribute("style", "border: 0");
-    themes[0].setAttribute("style", "border: 0");
+  for (i = 0; i < cells_dark.length; i++) {
+    cells_dark[i].setAttribute("style", "background-color: #ff6699");
+  }
+});
 
-    for(i=0; i<cells_light.length; i++){
-        cells_light[i].setAttribute("style", "background-color: white");
-    }
-
-    for(i=0; i<cells_dark.length; i++){
-        cells_dark[i].setAttribute("style", "background-color: #ff6699");
-    }
-})
-
-start_end.addEventListener("click", (event)=>{
-    if (event.target.style.backgroundColor == "red"){
-        event.target.style.backgroundColor = "green";
-        start_end.value = "Start";
-    }
-    else {
-        start_end.style.backgroundColor = "red";
-        start_end.value = "End";
-    }
-})
+//checks for button click on start button (start button used to begin the game and start player timer)
+start_end.addEventListener("click", (event) => {
+  if (event.target.style.backgroundColor == "red") {
+    event.target.style.backgroundColor = "green";
+    start_end.value = "Start";
+  } else {
+    start_end.style.backgroundColor = "red";
+    start_end.value = "End";
+  }
+});
