0f0d8c7a12b52f01b293d0a6d4cde4719d417737
TobiLean <Tobiloba.tsl@gmail.com>
2023-12-11 13:52:24 +0400

    Fixed game page, added cellId and draggable pieces

--- js/game.js
+++ js/game.js
@@ -1,4 +1,5 @@
-const cell = document.getElementsByClassName("cell");
+const cells = document.querySelectorAll(".cell");
+console.log(cells)
 
 const INITIAL_PIECE_POSITION = [
     ROOK, KNIGHT, BISHOP, KING, QUEEN, BISHOP, KNIGHT, ROOK,
@@ -12,19 +13,48 @@ const INITIAL_PIECE_POSITION = [
     
 ]
 
+const columns = [
+    "a", "b", "c", "d", "e", "f", "g", "h"
+];
+
+const rows = [
+    8, 7, 6, 5, 4, 3, 2, 1
+];
+
 function setPieces() {
     INITIAL_PIECE_POSITION.forEach((el, i) => {
-        cell[i].innerHTML = INITIAL_PIECE_POSITION[i];
-        cell[i].firstChild?.setAttribute('draggable', true)
+        cells[i].innerHTML = INITIAL_PIECE_POSITION[i];
+        cells[i].firstChild?.setAttribute('draggable', true)
 
         if(i < 16){
-            cell[i].firstChild.firstChild.classList.add('dark_piece')
+            cells[i].firstChild.firstChild.classList.add('dark_piece')
         }
 
         if(i > 47){
-            cell[i].firstChild.firstChild.classList.add('light_piece')
+            cells[i].firstChild.firstChild.classList.add('light_piece')
         }
     });
 }
 
+function setCellId(){
+    var rowNum = 0
+
+    cells.forEach((cell, i) => {
+        if (i%8 == 0){
+            rowNum++;
+        }
+
+        cell.setAttribute("cellId", `${columns[i%8]}${rows[rowNum-1]}`);
+    })
+}
+
+setCellId();
 setPieces();
+
+cells.forEach(cell => {
+    cell.addEventListener('dragstart', dragPiece)
+})
+
+function dragPiece(event){
+    console.log(event.target.parentNode);
+}
